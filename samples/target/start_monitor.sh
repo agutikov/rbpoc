dd3b32a6bc2c515a9992965d20a4eb28b4443f95
Alex Gutikov <gutikoff@gmail.com>
2016-11-09 00:11:14 +0300

    configure ads1015 before start reading

--- samples/target/start_monitor.sh
+++ samples/target/start_monitor.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 
+# HOST=192.168.128.190
 HOST=192.168.1.241
 PORT=9999
 
 insmod ./ads1015_adc.ko
 echo ads1015_adc 0x48 > /sys/bus/i2c/devices/i2c-1/new_device
+
+echo 6 > /sys/bus/i2c/drivers/ads1015_adc/1-0048/ads1015_adc_config/channel
+echo 1 > /sys/bus/i2c/drivers/ads1015_adc/1-0048/ads1015_adc_config/gain
+echo 3 > /sys/bus/i2c/drivers/ads1015_adc/1-0048/ads1015_adc_config/rate
+
+
 
 ./monitor_ads1015_adc.py $HOST $PORT
 
